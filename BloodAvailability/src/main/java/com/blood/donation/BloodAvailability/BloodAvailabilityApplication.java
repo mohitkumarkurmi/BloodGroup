209f41e041e41fa8a36bea03f8154ee1c2786490
Mohit <jabalpurmohit@gmail.com>
2018-09-27 16:48:34 +0530

    first commit

---
+++ BloodAvailability/src/main/java/com/blood/donation/BloodAvailability/BloodAvailabilityApplication.java
@@ -0,0 +1,12 @@
+package com.blood.donation.BloodAvailability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodAvailabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodAvailabilityApplication.class, args);
+	}
+}
